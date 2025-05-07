9df48363d5ca3386c8ce030f83b11bb452814599
tplansky <tomas.plansky@generalbytes.com>
2025-05-07 11:29:54 +0200

    BATM-7282 Add hashes to received amount in NanoNodeWallet

--- server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/nano/rpc/dto/Block.java
+++ server_extensions_extra/src/main/java/com/generalbytes/batm/server/extensions/extra/nano/rpc/dto/Block.java
@@ -2,5 +2,5 @@ package com.generalbytes.batm.server.extensions.extra.nano.rpc.dto;
 
 import java.math.BigInteger;
 
-public record Block(String type, String account, BigInteger amount) {
+public record Block(String type, String account, BigInteger amount, String hash) {
 }
